0775a663420f1b1444a0892de3bdd49da0c1ae21
martin14527 <guillermovallejoloya@gmail.com>
2025-10-03 22:26:23 -0600

    Add files via upload

---
+++ EVA2_6_ARREGLOS/src/eva2_6_arreglos/EVA2_6_ARREGLOS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_6_arreglos;
+
+/**
+ *
+ * @author 52614
+ */
+public class EVA2_6_ARREGLOS {
+
+    public static void main(String[] args) {
+  String cade="hola mundo";
+  System.out.println(cade.charAt(0));
+  char caracter = cade.charAt(11);
+  System.out.println(cade.length());
+  int valor = 10;
+ 
+    }
+   
+}
---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,21 @@
+package eva2_8_for;
+
+/**
+ *
+ * @author 52614
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { //hazta donde llegas 
+        
+       //añadir o resta ++ o --
+       for(int i = 10; i >1; i--){         
+   System.out.println(i);
+           }
+       }
+    
+    }
+
