6c49fac972cb427725b8448d9c4c48dff00c9824
martin14527 <guillermovallejoloya@gmail.com>
2026-03-12 18:08:10 -0600

    Add files via upload

---
+++ EVA2_1_HERENCIA/src/eva2_1_herencia/EVA2_1_HERENCIA.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_herencia;
+
+/**
+ *
+ * @author anoni
+ */
+public class EVA2_1_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejemplo ejemplo = new Ejemplo();
+        System.out.print("ejemplo.toString()");
+    }
+    
+}
+    class Ejemplo{
+}
